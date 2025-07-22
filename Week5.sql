42fbb2be0ff27393e05cd383cc187e586f99fcbf
WessleyJames <wessleywaweru@gmail.com>
2025-07-22 18:32:41 +0300

    Add Week5.sql for Week 5 assignment (indexes and user management)

---
+++ Week5.sql
@@ -0,0 +1,17 @@
+-- ✅ Week 5 Assignment
+-- Indexes, user management, and privileges.
+
+-- Question 1
+CREATE INDEX IdxTotalAmount ON bills(TotalAmount);
+
+-- Question 2
+DROP INDEX IdxEmail ON customer;
+
+-- Question 3
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 4
+GRANT INSERT ON bills.* TO 'bob'@'localhost';
+
+-- Question 5
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
